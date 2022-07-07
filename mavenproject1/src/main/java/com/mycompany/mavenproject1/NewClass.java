506b9cfd830f0e684fdb6450fe604459d59e7900
Arvind verma <Arvind verma@DESKTOP-EHKSVK6>
2022-07-07 09:08:51 +0530

    Java 8 stream

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -4,10 +4,54 @@
  */
 package com.mycompany.mavenproject1;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author Arvind verma
  */
 public class NewClass {
+
+    public static void main(String[] args) {
+
+        Integer arr[] = {10, 3, 4, 5, 10, 20, 8, 4, 39, 41, 20};
+        List<Integer> list = Arrays.asList(arr);
+
+        //remove duplicate Element in the list
+        System.out.println("remove duplicate Element in the list");
+        Set<Integer> removeDuplicate = new HashSet<Integer>();
+        list.stream().filter(d -> removeDuplicate.add(d)).collect(Collectors.toSet());
+        System.out.println(removeDuplicate);
+
+        //display duplicate item only
+        System.out.println("display duplicate item only");
+        Set<Integer> duplicate = new HashSet<Integer>();
+        list.stream().filter(num -> !duplicate.add(num)).collect(Collectors.toSet());
+
+        //Display sum of all odd number
+        System.out.println("Display sum of all odd number");
+        System.out.println(list.stream().filter(num -> num % 2 != 0).reduce(0, Integer::sum));
+
+        //Display multiply of all odd number
+        System.out.println("Display sum of all even number");
+        System.out.println(list.stream().filter(num -> num % 2 == 0).reduce(0, Integer::sum));
+
+        String name[] = {"Arvind", "Raja", "Rani", "Priya", "Ansu", "Akash", "Priya", "Arvind", "Raj", "Raja"};
+        List<String> listName = Arrays.asList(name);
+
+        //Find Number of word that are repated.
+        System.out.println(listName.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
+
+        //Remove duplicate word from the list
+        System.out.println("Remove duplicate word from the list");
+        Set<String> removeDuplicateName = new HashSet<String>();
+        listName.stream().filter(pname -> !removeDuplicateName.add(pname)).collect(Collectors.toSet());
+        System.out.println(removeDuplicateName);
+    }
 
 }
