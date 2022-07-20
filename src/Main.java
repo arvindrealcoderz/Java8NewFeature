3bb1c7384eb8ac3b236ccf228bc0534e5d21f88d
Arvind7238 <Arvind@buddy4study.com>
2022-07-20 17:48:37 +0530

    Java Application

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hehnsdfhdvdsfhld!");
+        List<String> list1 = Arrays.asList("A", "B", "C");
+        List<String> list2 = Arrays.asList("A", "B", "C");
+
+        list1.stream().forEach(data -> {
+
+            System.out.println(data);
+        });
+
     }
 }
