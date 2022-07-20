7ace9a1e264d6f9adbdc1fae1f15af236e6c7824
Arvind7238 <Arvind@buddy4study.com>
2022-07-20 17:56:53 +0530

    Java Application

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,12 @@ public class Main {
         List<String> list2 = Arrays.asList("A", "B", "C");
 
         list1.stream().forEach(data -> {
-
             System.out.println(data);
         });
+        System.out.println("----------------------");
+        list2.stream().filter(data -> data.equalsIgnoreCase("A")).forEach(d ->
+                System.out.println(d)
+        );
 
     }
 }
